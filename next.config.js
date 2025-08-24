be7fa8aba594aa72dbdc6fa323def700ffd27e19
Adoraq1 <paul@adoraq.com>
2025-08-24 19:40:49 +0100

    Simplify next.config.js: remove custom webpack config to fix Vercel build

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  // Ensure proper module resolution
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': require('path').resolve(__dirname),
-    }
-    return config
   }
 }
 
